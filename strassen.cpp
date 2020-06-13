ffc97ad1b98e2f832372424a7c38ca063df59df9
Bernal <berrojas45@gmail.com>
2020-06-12 21:21:15 -0600

    changes

--- strassen.cpp
+++ strassen.cpp
@@ -4,6 +4,7 @@
  * BCD-9218: Parallel and Distributed Computing
  * Instructor Diego Jimenez, Eng. (diego.jimenez@ulead.ac.cr)
  * OpenMP parallel Strassen algorithm for matrix multiplicationplication.
+ * Modificado por Bernal Rojas
  */
 
 #include <cstdio>
@@ -12,55 +13,52 @@
 #include "/usr/local/opt/libomp/include/omp.h"
 #include <iostream>
 #include "io.h"
-#include <vector>
+#include <vector> 
 #include <cmath>
 #include <algorithm>
 using namespace std;
 
 
-void sum(vector<vector<int> > &A, vector<vector<int> > &B, vector<vector<int> > &C, int size)
-{
-    int i, j;
+//----------------- Funciones auxiliares para calculos -----------------
+//* sum: Matrix Sum
+//* subtraction: Matrix substraction
+//* multiplication: Matrix multiplication
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
+
+void sum(vector<vector<int> > &A, vector<vector<int> > &B, vector<vector<int> > &C, int size){
+    int i, j;
+    for (i = 0; i < size; i++){
+        for (j = 0; j < size; j++){
             C[i][j] = A[i][j] + B[i][j];
         }
     }
 }
 
-void subtraction(vector<vector<int> > &A, vector<vector<int> > &B, vector<vector<int> > &C, int size)
-{
+void subtraction(vector<vector<int> > &A, vector<vector<int> > &B, vector<vector<int> > &C, int size){
     int i, j;
-
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
+    for (i = 0; i < size; i++){
+        for (j = 0; j < size; j++){
             C[i][j] = A[i][j] - B[i][j];
         }
     }
 }
 
-void multiplication(vector<vector<int> > &A, vector<vector<int> > &B, vector<vector<int> > &C, int size)
-{
+void multiplication(vector<vector<int> > &A, vector<vector<int> > &B, vector<vector<int> > &C, int size){
     int i, j, k;
     for(i = 0; i < size; ++i)
         for(j = 0; j < size; ++j)
-            for(k = 0; k < size; ++k)
-            {
+            for(k = 0; k < size; ++k){
                 C[i][j] += A[i][k] * B[k][j];
             }
 }
 
 
-// TODO: Function implementing Strassen's algorithm
+//----------------- Algoritmo Strassen -----------------
 void strassen(int **A, int **B, int **C, int N) {
 
-    // TODO: YOUR CODE GOES HERE
     int new_size = N / 2;
+    int i, j;
+
     vector<int> z(new_size);
     vector<vector<int> >
             a11(new_size, z), a12(new_size, z), a21(new_size, z), a22(new_size, z),
@@ -69,10 +67,9 @@ void strassen(int **A, int **B, int **C, int N) {
             M1(new_size, z), M2(new_size, z), M3(new_size, z), M4(new_size, z),
             M5(new_size, z), M6(new_size, z), M7(new_size, z),
             aResult(new_size, z), bResult(new_size, z);
+            
+  
 
-    int i, j;
-
-//dividing the matrices into subtraction-matrices:
     for (i = 0; i < new_size; i++) {
         for (j = 0; j < new_size; j++) {
             a11[i][j] = A[i][j];
@@ -87,63 +84,43 @@ void strassen(int **A, int **B, int **C, int N) {
         }
     }
 
+// Paralelizar calculo
 #pragma omp parallel
-    {
+{
 #pragma omp single
         {
-
             sum(a11, a22, aResult, new_size);
             sum(b11, b22, bResult, new_size);
             multiplication(aResult, bResult, M1, new_size);
-
-
-            sum(a21, a22, aResult, new_size); // a21 + a22
+            sum(a21, a22, aResult, new_size); 
             multiplication(aResult, b11, M2, new_size);
-            // p2 = (a21+a22) * (b11)
-
-            subtraction(b12, b22, bResult, new_size);      // b12 - b22
+            subtraction(b12, b22, bResult, new_size);     
             multiplication(a11, bResult, M3, new_size);
-            // p3 = (a11) * (b12 - b22)
-
-            subtraction(b21, b11, bResult, new_size);       // b21 - b11
+            subtraction(b21, b11, bResult, new_size);     
             multiplication(a22, bResult, M4, new_size);
-            // p4 = (a22) * (b21 - b11)
-
-            sum(a11, a12, aResult, new_size);      // a11 + a12
+            sum(a11, a12, aResult, new_size);    
             multiplication(aResult, b22, M5, new_size);
-            // p5 = (a11+a12) * (b22)
-
-            subtraction(a21, a11, aResult, new_size);      // a21 - a11
+            subtraction(a21, a11, aResult, new_size);     
             sum(b11, b12, bResult, new_size);
-            // b11 + b12
             multiplication(aResult, bResult, M6, new_size);
-            // p6 = (a21-a11) * (b11+b12)
-
-            subtraction(a12, a22, aResult, new_size);      // a12 - a22
+            subtraction(a12, a22, aResult, new_size);     
             sum(b21, b22, bResult, new_size);
-            // b21 + b22
             multiplication(aResult, bResult, M7, new_size);
-            // p7 = (a12-a22) * (b21+b22)
         }
     }
 
-    // calculating c21, c21, c11 e c22:
+    // Construir C
+    sum(M3, M5, c12, new_size);
+    sum(M2, M4, c21, new_size); 
+    sum(M1, M4, aResult, new_size);      
+    sum(aResult, M7, bResult, new_size);  
+    subtraction(bResult, M5, c11, new_size); 
+    sum(M1, M3, aResult, new_size);      
+    sum(aResult, M6, bResult, new_size); 
+    subtraction(bResult, M2, c22, new_size);
 
-    sum(M3, M5, c12, new_size); // c12 = p3 + p5
-    sum(M2, M4, c21, new_size); // c21 = p2 + p4
-
-    sum(M1, M4, aResult, new_size);       // p1 + p4
-    sum(aResult, M7, bResult, new_size);  // p1 + p4 + p7
-    subtraction(bResult, M5, c11, new_size); // c11 = p1 + p4 - p5 + p7
-
-    sum(M1, M3, aResult, new_size);       // p1 + p3
-    sum(aResult, M6, bResult, new_size);  // p1 + p3 + p6
-    subtraction(bResult, M2, c22, new_size); // c22 = p1 + p3 - p2 + p6
-
-    for (i = 0; i < new_size; i++)
-    {
-        for (j = 0; j < new_size; j++)
-        {
+    for (i = 0; i < new_size; i++) {
+        for (j = 0; j < new_size; j++){
             C[i][j] = c11[i][j];
             C[i][j + new_size] = c12[i][j];
             C[i + new_size][j] = c21[i][j];
@@ -154,9 +131,7 @@ void strassen(int **A, int **B, int **C, int N) {
 
 
 
-
-
-// Main method
+//----------------- Main -----------------
 int main(int argc, char* argv[]) {
 	int N;
 	int **A, **B, **C;
@@ -188,7 +163,8 @@ int main(int argc, char* argv[]) {
 	// starting timer
 	timerStart();
 
-	// TODO: YOUR CODE GOES HERE
+	
+    strassen(A, B, C, N); // Llamada a la función
 
 
 	// testing the results is correct
